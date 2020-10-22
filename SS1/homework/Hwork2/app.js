8b794fd013d4a8a6ec4b92592f49db2852e34ff2
nguyenvanliem0501 <nguyenvanliem0501@gmail.com>
2020-10-23 00:46:54 +0700

    zo

--- SS1/homework/Hwork2/app.js
+++ SS1/homework/Hwork2/app.js
@@ -1,10 +1,12 @@
 var a = [1, 2, 3, 4, 5];
-let input = Number(prompt("Nhap vao so bat ki"));
+var  x = Number(prompt("Nhap vao so x bat ki: "));
 for(let i=0; i<a.length;i++){
     for(let j=i+1;j<a.length;j++){
-        var k = Number(a[i] + a[j]);
-        if(k == input){
+        var check= Number(a[i] + a[j]);
+        if(check == x){
             console.log(`(${i} , ${j})`);
+        }else{
+            console.log("Khong co gia tri");
         }
     }
 }
